712f525fc7ed050eb3832f3e7e3f95178ca0efe5
HRISHIKESH SAPKAL <132556772+HRISHIXP@users.noreply.github.com>
2024-05-20 18:50:26 +0530

    first commit

---
+++ frontend/src/components/shared/TextWithHover.js
@@ -0,0 +1,16 @@
+const TextWithHover = ({displayText, active, onClick}) => {
+    return (
+        <div className="flex items-center justify-start cursor-pointer">
+            <div
+                className={`${
+                    active ? "text-white" : "text-gray-500"
+                } font-semibold hover:text-white`}
+                onClick={onClick}
+            >
+                {displayText}
+            </div>
+        </div>
+    );
+};
+
+export default TextWithHover;
